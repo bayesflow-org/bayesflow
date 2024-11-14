5cb5847abe13a3b7eb663ca66749e55789af9e05
Jerry <aca.jerryh@gmail.com>
2024-11-13 23:20:03 -0500

    Update and test plot_sbc_histograms

--- bayesflow/diagnostics/plot_sbc_histograms.py
+++ bayesflow/diagnostics/plot_sbc_histograms.py
@@ -1,25 +1,28 @@
 import logging
+
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+from typing import Sequence
 from scipy.stats import binom
-from ..utils.plot_utils import preprocess, add_labels
+from ..utils.plot_utils import preprocess, add_labels, prettify_subplots
 
 
 def plot_sbc_histograms(
-    post_samples,
-    prior_samples,
-    param_names=None,
-    fig_size=None,
-    num_bins=None,
+    post_samples: dict[str, np.ndarray] | np.ndarray,
+    prior_samples: dict[str, np.ndarray] | np.ndarray,
+    names: Sequence[str] = None,
+    figsize: Sequence[int] = None,
+    num_bins: int = 10,
     binomial_interval=0.99,
-    label_fontsize=16,
-    title_fontsize=18,
-    tick_fontsize=12,
-    hist_color="#a34f4f",
-    n_row=None,
-    n_col=None,
-):
+    label_fontsize: int = 16,
+    title_fontsize: int = 18,
+    tick_fontsize: int = 12,
+    color: str = "#132a70",
+    num_col: int = None,
+    num_row: int = None,
+) -> plt.Figure:
     """Creates and plots publication-ready histograms of rank statistics for simulation-based calibration
     (SBC) checks according to [1].
 
@@ -36,9 +39,9 @@ def plot_sbc_histograms(
         The posterior draws obtained from n_data_sets
     prior_samples     : np.ndarray of shape (n_data_sets, n_params)
         The prior draws obtained for generating n_data_sets
-    param_names       : list or None, optional, default: None
+    names       : list or None, optional, default: None
         The parameter names for nice plot titles. Inferred if None
-    fig_size          : tuple or None, optional, default : None
+    figsize          : tuple or None, optional, default : None
         The figure size passed to the matplotlib constructor. Inferred if None
     num_bins          : int, optional, default: 10
         The number of bins to use for each marginal histogram
@@ -50,11 +53,11 @@ def plot_sbc_histograms(
         The font size of the title text
     tick_fontsize     : int, optional, default: 12
         The font size of the axis ticklabels
-    hist_color        : str, optional, default '#a34f4f'
+    color        : str, optional, default '#a34f4f'
         The color to use for the histogram body
-    n_row             : int, optional, default: None
+    num_row             : int, optional, default: None
         The number of rows for the subplots. Dynamically determined if None.
-    n_col             : int, optional, default: None
+    num_col             : int, optional, default: None
         The number of columns for the subplots. Dynamically determined if None.
 
     Returns
@@ -68,22 +71,14 @@ def plot_sbc_histograms(
     """
 
     # Preprocessing
-    f, ax_array, ax_array_it, n_row, n_col, n_params, param_names = preprocess(
-        post_samples=post_samples,
-        prior_samples=prior_samples,
-        n_col=n_col,
-        n_row=n_row,
-        param_names=param_names,
-        fig_size=fig_size,
-        flatten=False,
-    )
+    plot_data = preprocess(post_samples, prior_samples, num_col, num_row, names, figsize)
 
     # Determine the ratio of simulations to prior draws
-    # n_params = post_samples.shape[-1]
-    n_sim = post_samples.shape[0]
-    n_draws = post_samples.shape[1]
+    # num_params = plot_data['num_variables']
+    num_sims = plot_data['post_samples'].shape[0]
+    num_draws = plot_data['post_samples'].shape[1]
 
-    ratio = int(n_sim / n_draws)
+    ratio = int(num_sims / num_draws)
 
     # Log a warning if N/B ratio recommended by Talts et al. (2018) < 20
     if ratio < 20:
@@ -103,25 +98,26 @@ def plot_sbc_histograms(
             num_bins = 5
 
     # Compute ranks (using broadcasting)
-    ranks = np.sum(post_samples < prior_samples[:, np.newaxis, :], axis=1)
+    ranks = np.sum(plot_data['post_samples'] < plot_data['prior_samples'][:, np.newaxis, :], axis=1)
 
     # Compute confidence interval and mean
-    N = int(prior_samples.shape[0])
+    num_trials = int(plot_data['prior_samples'].shape[0])
     # uniform distribution expected -> for all bins: equal probability
     # p = 1 / num_bins that a rank lands in that bin
-    endpoints = binom.interval(binomial_interval, N, 1 / num_bins)
-    mean = N / num_bins  # corresponds to binom.mean(N, 1 / num_bins)
+    endpoints = binom.interval(binomial_interval, num_trials, 1 / num_bins)
+    mean = num_trials / num_bins  # corresponds to binom.mean(N, 1 / num_bins)
 
     # Plot marginal histograms in a loop
-    if n_row > 1:
-        ax = ax_array.flat
+    if plot_data['num_row'] > 1:
+        ax = plot_data["axes"].flat
     else:
-        ax = ax_array
-    for j in range(len(param_names)):
+        ax = plot_data["axes"]
+
+    for j in range(len(plot_data['names'])):
         ax[j].axhspan(endpoints[0], endpoints[1], facecolor="gray", alpha=0.3)
         ax[j].axhline(mean, color="gray", zorder=0, alpha=0.9)
-        sns.histplot(ranks[:, j], kde=False, ax=ax[j], color=hist_color, bins=num_bins, alpha=0.95)
-        ax[j].set_title(param_names[j], fontsize=title_fontsize)
+        sns.histplot(ranks[:, j], kde=False, ax=ax[j], color=color, bins=num_bins, alpha=0.95)
+        ax[j].set_title(plot_data['names'][j], fontsize=title_fontsize)
         ax[j].spines["right"].set_visible(False)
         ax[j].spines["top"].set_visible(False)
         ax[j].get_yaxis().set_ticks([])
@@ -129,12 +125,17 @@ def plot_sbc_histograms(
         ax[j].tick_params(axis="both", which="major", labelsize=tick_fontsize)
         ax[j].tick_params(axis="both", which="minor", labelsize=tick_fontsize)
 
+    # Prettify
+    prettify_subplots(plot_data['axes'], tick_fontsize)
+
     # Only add x-labels to the bottom row
-    add_labels(ax_array=ax_array, n_row=n_row, n_col=n_col, x_label="Rank statistic", label_fontsize=label_fontsize)
+    add_labels(
+        axes=plot_data['axes'],
+        num_row=plot_data['num_row'],
+        num_col=plot_data['num_col'],
+        xlabel="Rank statistic",
+        label_fontsize=label_fontsize
+    )
 
-    # Remove unused axes entirely
-    for _ax in ax_array[n_params:]:
-        _ax.remove()
-
-    f.tight_layout()
-    return f
+    plot_data['fig'].tight_layout()
+    return plot_data['fig']

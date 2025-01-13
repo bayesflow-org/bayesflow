95a85286b70c835843afb49dac0e28989a34d6b2
LarsKue <lars@kuehmichel.de>
2025-01-13 15:05:42 +0100

    add rq spline function

---
+++ bayesflow/networks/coupling_flow/transforms/_rational_quadratic.py
@@ -0,0 +1,75 @@
+import keras
+from typing import TypedDict
+
+from bayesflow.types import Tensor
+
+
+class Edges(TypedDict):
+    left: Tensor
+    right: Tensor
+    bottom: Tensor
+    top: Tensor
+
+
+class Derivatives(TypedDict):
+    left: Tensor
+    right: Tensor
+
+
+def _rational_quadratic_spline(
+    x: Tensor, edges: Edges, derivatives: Derivatives, inverse: bool = False
+) -> (Tensor, Tensor):
+    # rename variables to match the paper:
+
+    # $x^{(k)}$
+    xk = edges["left"]
+
+    # $x^{(k+1)}$
+    xkp = edges["right"]
+
+    # $y^{(k)}$
+    yk = edges["bottom"]
+
+    # $y^{(k+1)}$
+    ykp = edges["top"]
+
+    # $delta^{(k)}$
+    dk = derivatives["left"]
+
+    # $delta^{(k+1)}$
+    dkp = derivatives["right"]
+
+    # commonly used values
+    dx = xkp - xk
+    dy = ykp - yk
+    sk = dy / dx
+
+    if not inverse:
+        xi = (x - xk) / dx
+
+        # Eq. 4 in the paper
+        numerator = dy * (sk * xi**2 + dk * xi * (1 - xi))
+        denominator = sk + (dkp + dk - 2 * sk) * xi * (1 - xi)
+        out = yk + numerator / denominator
+    else:
+        y = x
+        # Eq. 6-8 in the paper
+        a = dy * (sk - dk) + (y - yk) * (dkp + dk - 2 * sk)
+        b = dy * dk - (y - yk) * (dkp + dk - 2 * sk)
+        c = -sk * (y - yk)
+
+        # Eq. 29 in the appendix of the paper
+        discriminant = b**2 - 4 * a * c
+        if not keras.ops.all(discriminant >= 0):
+            raise ValueError("Discriminant must be non-negative.")
+
+        xi = 2 * c / (-b - keras.ops.sqrt(discriminant))
+
+        out = xi * dx + xk
+
+    # Eq 5 in the paper
+    numerator = sk**2 * (dkp * xi**2 + 2 * sk * xi * (1 - xi) + dk * (1 - xi) ** 2)
+    denominator = (sk + (dkp + dk - 2 * sk) * xi * (1 - xi)) ** 2
+    log_jac = keras.ops.log(numerator) - keras.ops.log(denominator)
+
+    return out, log_jac

b3f414362bc1db08c712651f895ab925c3288320
larskue <lars@kuehmichel.de>
2025-02-07 16:24:15 +0100

    fix negative step size integration fix small deviations in some backends add (required) min and max step number selection for dynamic integration improve dispatch remove step size selection (users can compute this if they need it, but exposing the argument is ambiguous w.r.t. fixed vs adaptive integration)

--- bayesflow/utils/integrate.py
+++ bayesflow/utils/integrate.py
@@ -1,6 +1,5 @@
 from collections.abc import Callable
 from functools import partial
-from numbers import Number
 
 import keras
 
@@ -11,91 +10,59 @@ from . import logging
 ArrayLike = int | float | Tensor
 
 
-def fixed_euler(
-    fn: Callable, state: dict[str, ArrayLike], time: ArrayLike, step_size: ArrayLike
-) -> (dict[str, ArrayLike], ArrayLike, ArrayLike):
-    state_deltas = fn(time, **filter_kwargs(state, fn))
-
-    for key, value in state_deltas.items():
-        state[key] += step_size * value
-
-    time += step_size
-
-    return state, time, step_size
-
-
-def dynamic_euler(
-    fn: Callable, state: dict[str, ArrayLike], time: ArrayLike, step_size: ArrayLike, tolerance: ArrayLike = 1e-3
-) -> (dict[str, ArrayLike], ArrayLike, ArrayLike):
-    deltas = fn(time, **filter_kwargs(state, fn))
-
-    intermediate_state = state.copy()
-    for key, delta in deltas.items():
-        intermediate_state[key] += step_size * delta
-
-    intermediate_deltas = fn(time + step_size, **filter_kwargs(intermediate_state, fn))
-
-    # check all keys are equal
-    if set(deltas.keys()) != set(intermediate_deltas.keys()):
-        raise ValueError("Keys of the deltas do not match. Please return zero for unchanged variables.")
-
-    # apply updates
-    for key, value in state.items():
-        state[key] += step_size * value
-
-    time += step_size
-
-    # compute next step size
-    intermediate_error = keras.ops.stack(
-        [keras.ops.norm(intermediate_deltas[key] - deltas[key], ord=2, axis=-1) for key in deltas]
-    )
-    step_size = step_size * tolerance / intermediate_error
-
-    # consolidate step size
-    step_size = keras.ops.take(step_size, keras.ops.argmin(keras.ops.abs(step_size)))
-
-    return state, time, step_size
-
-
-def fixed_rk45(
-    fn: Callable, state: dict[str, ArrayLike], time: ArrayLike, step_size: ArrayLike
+def euler_step(
+    fn: Callable,
+    state: dict[str, ArrayLike],
+    time: ArrayLike,
+    step_size: ArrayLike,
+    tolerance: ArrayLike = 1e-6,
+    min_step_size: ArrayLike = -float("inf"),
+    max_step_size: ArrayLike = float("inf"),
+    use_adaptive_step_size: bool = False,
 ) -> (dict[str, ArrayLike], ArrayLike, ArrayLike):
     k1 = fn(time, **filter_kwargs(state, fn))
 
-    # TODO: this can probably be done nicer somehow (notably not with a loop)
-    intermediate_state = state.copy()
-    for key, delta in k1.items():
-        intermediate_state[key] += 0.5 * step_size * delta
+    if use_adaptive_step_size:
+        intermediate_state = state.copy()
+        for key, delta in k1.items():
+            intermediate_state[key] = state[key] + step_size * delta
 
-    k2 = fn(time + 0.5 * step_size, **filter_kwargs(intermediate_state, fn))
+        k2 = fn(time + step_size, **filter_kwargs(intermediate_state, fn))
 
-    intermediate_state = state.copy()
-    for key, delta in k2.items():
-        intermediate_state[key] += 0.5 * step_size * delta
+        # check all keys are equal
+        if set(k1.keys()) != set(k2.keys()):
+            raise ValueError("Keys of the deltas do not match. Please return zero for unchanged variables.")
 
-    k3 = fn(time + 0.5 * step_size, **filter_kwargs(intermediate_state, fn))
+        # compute next step size
+        intermediate_error = keras.ops.stack([keras.ops.norm(k2[key] - k1[key], ord=2, axis=-1) for key in k1])
+        new_step_size = step_size * tolerance / (intermediate_error + 1e-9)
 
-    intermediate_state = state.copy()
-    for key, delta in k3.items():
-        intermediate_state[key] += step_size * delta
+        new_step_size = keras.ops.clip(new_step_size, min_step_size, max_step_size)
 
-    k4 = fn(time + step_size, **filter_kwargs(intermediate_state, fn))
-
-    # check all keys are equal
-    if not all(set(k.keys()) == set(k1.keys()) for k in [k2, k3, k4]):
-        raise ValueError("Keys of the deltas do not match. Please return zero for unchanged variables.")
+        # consolidate step size
+        new_step_size = keras.ops.take(new_step_size, keras.ops.argmin(keras.ops.abs(new_step_size)))
+    else:
+        new_step_size = step_size
 
     # apply updates
-    for key in k4.keys():
-        state[key] += (step_size / 6.0) * (k1[key] + 2.0 * k2[key] + 2.0 * k3[key] + k4[key])
+    new_state = state.copy()
+    for key in k1.keys():
+        new_state[key] = state[key] + step_size * k1[key]
 
-    time += step_size
+    new_time = time + step_size
 
-    return state, time, step_size
+    return new_state, new_time, new_step_size
 
 
-def dynamic_rk45(
-    fn: Callable, state: dict[str, ArrayLike], time: ArrayLike, last_step_size: ArrayLike, tolerance: ArrayLike = 1e-3
+def rk45_step(
+    fn: Callable,
+    state: dict[str, ArrayLike],
+    time: ArrayLike,
+    last_step_size: ArrayLike,
+    tolerance: ArrayLike = 1e-6,
+    min_step_size: ArrayLike = -float("inf"),
+    max_step_size: ArrayLike = float("inf"),
+    use_adaptive_step_size: bool = False,
 ) -> (dict[str, ArrayLike], ArrayLike, ArrayLike):
     step_size = last_step_size
 
@@ -103,109 +70,151 @@ def dynamic_rk45(
 
     intermediate_state = state.copy()
     for key, delta in k1.items():
-        intermediate_state[key] += 0.5 * step_size * delta
+        intermediate_state[key] = state[key] + 0.5 * step_size * delta
 
     k2 = fn(time + 0.5 * step_size, **filter_kwargs(intermediate_state, fn))
 
     intermediate_state = state.copy()
     for key, delta in k2.items():
-        intermediate_state[key] += 0.5 * step_size * delta
+        intermediate_state[key] = state[key] + 0.5 * step_size * delta
 
     k3 = fn(time + 0.5 * step_size, **filter_kwargs(intermediate_state, fn))
 
     intermediate_state = state.copy()
     for key, delta in k3.items():
-        intermediate_state[key] += step_size * delta
+        intermediate_state[key] = state[key] + step_size * delta
 
     k4 = fn(time + step_size, **filter_kwargs(intermediate_state, fn))
 
-    intermediate_state = state.copy()
-    for key, delta in k4.items():
-        intermediate_state[key] += 0.5 * step_size * delta
+    if use_adaptive_step_size:
+        intermediate_state = state.copy()
+        for key, delta in k4.items():
+            intermediate_state[key] = state[key] + 0.5 * step_size * delta
 
-    k5 = fn(time + 0.5 * step_size, **filter_kwargs(intermediate_state, fn))
+        k5 = fn(time + 0.5 * step_size, **filter_kwargs(intermediate_state, fn))
 
-    # check all keys are equal
-    if not all(set(k.keys()) == set(k1.keys()) for k in [k2, k3, k4, k5]):
-        raise ValueError("Keys of the deltas do not match. Please return zero for unchanged variables.")
+        # check all keys are equal
+        if not all(set(k.keys()) == set(k1.keys()) for k in [k2, k3, k4, k5]):
+            raise ValueError("Keys of the deltas do not match. Please return zero for unchanged variables.")
+
+        # compute next step size
+        intermediate_error = keras.ops.stack([keras.ops.norm(k5[key] - k4[key], ord=2, axis=-1) for key in k5.keys()])
+        new_step_size = step_size * tolerance / (intermediate_error + 1e-9)
+
+        new_step_size = keras.ops.clip(new_step_size, min_step_size, max_step_size)
+
+        # consolidate step size
+        new_step_size = keras.ops.take(new_step_size, keras.ops.argmin(keras.ops.abs(new_step_size)))
+    else:
+        new_step_size = step_size
 
     # apply updates
-    for key in k5.keys():
-        state[key] += (step_size / 6.0) * (k1[key] + 2.0 * k2[key] + 2.0 * k3[key] + k4[key])
+    new_state = state.copy()
+    for key in k1.keys():
+        new_state[key] = state[key] + (step_size / 6.0) * (k1[key] + 2.0 * k2[key] + 2.0 * k3[key] + k4[key])
 
-    time += step_size
+    new_time = time + step_size
 
-    # compute next step size
-    intermediate_error = keras.ops.stack([keras.ops.norm(k5[key] - k4[key], ord=2, axis=-1) for key in k5.keys()])
-    step_size = step_size * tolerance / intermediate_error
-
-    # consolidate step size
-    step_size = keras.ops.take(step_size, keras.ops.argmin(keras.ops.abs(step_size)))
-
-    return state, time, step_size
+    return new_state, new_time, new_step_size
 
 
-def step_function_factory(fn: Callable, method: str, start_time, stop_time, steps, step_size):
-    match steps, step_size:
-        case "adaptive", "adaptive":
-            # TODO: select a better initial step size
-            step_size = (stop_time - start_time) / 100
-            use_adaptive_step_size = True
-        case int(), "adaptive":
-            step_size = (stop_time - start_time) / steps
-            use_adaptive_step_size = False
-        case "adaptive", Number():
-            use_adaptive_step_size = False
-        case int(), Number():
-            raise ValueError("Cannot specify both `steps` and `step_size`.")
-        case _:
-            raise RuntimeError("Type or value of `steps` or `step_size` not understood.")
+def integrate_fixed(
+    fn: Callable,
+    state: dict[str, ArrayLike],
+    start_time: ArrayLike,
+    stop_time: ArrayLike,
+    steps: int,
+    method: str = "rk45",
+    **kwargs,
+) -> dict[str, ArrayLike]:
+    if steps <= 0:
+        raise ValueError("Number of steps must be positive.")
 
     match method:
         case "euler":
-            if use_adaptive_step_size:
-                step_fn = dynamic_euler
-            else:
-                step_fn = fixed_euler
+            step_fn = euler_step
         case "rk45":
-            if use_adaptive_step_size:
-                step_fn = dynamic_rk45
-            else:
-                step_fn = fixed_rk45
+            step_fn = rk45_step
         case str() as name:
             raise ValueError(f"Unknown integration method name: {name!r}")
         case other:
             raise TypeError(f"Invalid integration method: {other!r}")
 
-    step_fn = partial(step_fn, fn)
+    step_fn = partial(step_fn, fn, **kwargs, use_adaptive_step_size=False)
+    step_size = (stop_time - start_time) / steps
 
-    return step_fn, step_size
+    time = start_time
+
+    def body(_loop_var, _loop_state):
+        _state, _time = _loop_state
+        _state, _time, _ = step_fn(_state, _time, step_size)
+
+        return _state, _time
+
+    state, time = keras.ops.fori_loop(0, steps, body, (state, time))
+
+    return state
 
 
-def integrate(
+def integrate_adaptive(
     fn: Callable,
     state: dict[str, ArrayLike],
     start_time: ArrayLike,
     stop_time: ArrayLike,
-    steps: int = "adaptive",
-    step_size: Number = "adaptive",
+    min_steps: int = 10,
+    max_steps: int = 10_000,
     method: str = "rk45",
     **kwargs,
 ) -> dict[str, ArrayLike]:
-    step_fn, step_size = step_function_factory(fn, method, start_time, stop_time, steps, step_size)
+    if max_steps <= min_steps:
+        raise ValueError("Maximum number of steps must be greater than minimum number of steps.")
 
-    if kwargs:
-        step_fn = partial(step_fn, **kwargs)
+    match method:
+        case "euler":
+            step_fn = euler_step
+        case "rk45":
+            step_fn = rk45_step
+        case str() as name:
+            raise ValueError(f"Unknown integration method name: {name!r}")
+        case other:
+            raise TypeError(f"Invalid integration method: {other!r}")
 
-    def cond(state, time, step_size, step):
-        # step until the next step would exceed the stop time
-        return keras.ops.all(
-            keras.ops.where(stop_time > start_time, time + step_size <= stop_time, time + step_size >= stop_time)
+    step_fn = partial(step_fn, fn, **kwargs, use_adaptive_step_size=True)
+
+    def cond(_state, _time, _step_size, _step):
+        # while step < min_steps or time_remaining > 0 and step < max_steps
+
+        # time remaining after the next step
+        time_remaining = keras.ops.abs(stop_time - (_time + _step_size))
+
+        return keras.ops.logical_or(
+            keras.ops.all(_step < min_steps),
+            keras.ops.logical_and(keras.ops.all(time_remaining > 0), keras.ops.all(_step < max_steps)),
         )
 
-    def body(state, time, step_size, step):
-        state, time, step_size = step_fn(state, time, step_size)
-        return state, time, step_size, step + 1
+    def body(_state, _time, _step_size, _step):
+        _step = _step + 1
+
+        # time remaining after the next step
+        time_remaining = stop_time - (_time + _step_size)
+
+        min_step_size = time_remaining / (max_steps - _step)
+        max_step_size = time_remaining / keras.ops.maximum(min_steps - _step, 1)
+
+        # reorder
+        min_step_size, max_step_size = (
+            keras.ops.minimum(min_step_size, max_step_size),
+            keras.ops.maximum(min_step_size, max_step_size),
+        )
+
+        _state, _time, _step_size = step_fn(
+            _state, _time, _step_size, min_step_size=min_step_size, max_step_size=max_step_size
+        )
+
+        return _state, _time, _step_size, _step
+
+    # select initial step size conservatively
+    step_size = (stop_time - start_time) / max_steps
 
     step = 0
     time = start_time
@@ -215,8 +224,28 @@ def integrate(
     # do the last step
     step_size = stop_time - time
     state, _, _ = step_fn(state, time, step_size)
-    step += 1
+    step = step + 1
 
     logging.debug("Finished integration after {} steps.", step)
 
     return state
+
+
+def integrate(
+    fn: Callable,
+    state: dict[str, ArrayLike],
+    start_time: ArrayLike,
+    stop_time: ArrayLike,
+    min_steps: int = 10,
+    max_steps: int = 10_000,
+    steps: int = "adaptive",
+    method: str = "rk45",
+    **kwargs,
+) -> dict[str, ArrayLike]:
+    match steps:
+        case "adaptive":
+            return integrate_adaptive(fn, state, start_time, stop_time, min_steps, max_steps, method, **kwargs)
+        case int():
+            return integrate_fixed(fn, state, start_time, stop_time, steps, method, **kwargs)
+        case _:
+            raise RuntimeError("Type or value of `steps` not understood.")

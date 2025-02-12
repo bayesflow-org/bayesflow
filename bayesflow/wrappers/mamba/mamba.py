4c98c4ad35fac3de7fc4b573263ac8d1f0fd2fcf
Chase-Grajeda <chase.grajeda@gmail.com>
2025-02-11 19:20:42 -0500

    Update mamba.py
    Added activation between Mamba blocks.

--- bayesflow/wrappers/mamba/mamba.py
+++ bayesflow/wrappers/mamba/mamba.py
@@ -80,6 +80,7 @@ class MambaSSM(SummaryNetwork):
         summary = time_series
         for mamba_block in self.mamba_blocks:
             summary = mamba_block(summary, **kwargs)
+            summary = keras.activations.relu(summary) # TODO: custom activation
 
         if self.pooling:
             summary = self.pooling(summary)

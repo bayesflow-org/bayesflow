e57c7c8e0d2c06187c0d521206ded0a7246623e0
Chase-Grajeda <chase.grajeda@gmail.com>
2025-02-11 19:39:44 -0500

    Update mamba.py
    Switched activation to softplus

--- bayesflow/wrappers/mamba/mamba.py
+++ bayesflow/wrappers/mamba/mamba.py
@@ -80,7 +80,7 @@ class MambaSSM(SummaryNetwork):
         summary = time_series
         for mamba_block in self.mamba_blocks:
             summary = mamba_block(summary, **kwargs)
-            summary = keras.activations.sigmoid(summary) # TODO: custom activatiom
+            summary = keras.activations.softplus(summary) # TODO: custom activatiom
 
         if self.pooling:
             summary = self.pooling(summary)

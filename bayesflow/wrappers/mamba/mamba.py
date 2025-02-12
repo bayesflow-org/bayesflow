634e11efb9766ddf39d4129600f3f40a61378791
Chase-Grajeda <chase.grajeda@gmail.com>
2025-02-11 20:12:58 -0500

    Update mamba.py
    Fixed py syntax error

--- bayesflow/wrappers/mamba/mamba.py
+++ bayesflow/wrappers/mamba/mamba.py
@@ -74,14 +74,7 @@ class MambaSSM(SummaryNetwork):
         else:
             raise NotImplementedError("Mamba version must be 1 or 2")
         
-        self.mamba_blocks = [mamba_gen(
-            d_model=ssm_dim,
-            d_state=state_dim,
-            d_conv=conv_dim,
-            expand=expand,
-            dt_min=dt_min,
-            dt_max=dt_max
-        ).to(device) for _ in range(mamba_blocks)]
+        self.mamba_blocks = [mamba_gen(d_model=ssm_dim, d_state=state_dim, d_conv=conv_dim, expand=expand, dt_min=dt_min, dt_max=dt_max).to(device) for _ in range(mamba_blocks)]
         
         self.pooling = pooling
         if pooling:

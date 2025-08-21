bd71c86c16410ddb7c823a285d7aec056db648b7
Valentin Pratz <git@valentinpratz.de>
2025-08-21 18:06:37 +0000

    adapt output dim of invariant module in equivariant module
    See #557. The DeepSet showed bad performance and was not able to learn
    diverse summary statistics. Reducing the dimension of the output of the
    invariant module inside the equivariant module improves this, probably
    because the invidividual information of each set member gains importance
    compared to the shared information provided by the invariant module.
    
    There might be better settings for this, so we might update the default
    later on. However, this is already an improvement over the previous
    setting.

--- bayesflow/networks/deep_set/deep_set.py
+++ bayesflow/networks/deep_set/deep_set.py
@@ -28,7 +28,7 @@ class DeepSet(SummaryNetwork):
         output_pooling: str = "mean",
         mlp_widths_equivariant: Sequence[int] = (64, 64),
         mlp_widths_invariant_inner: Sequence[int] = (64, 64),
-        mlp_widths_invariant_outer: Sequence[int] = (64, 64),
+        mlp_widths_invariant_outer: Sequence[int] = (64, 4),
         mlp_widths_invariant_last: Sequence[int] = (64, 64),
         activation: str = "silu",
         kernel_initializer: str = "he_normal",
@@ -68,7 +68,7 @@ class DeepSet(SummaryNetwork):
         mlp_widths_invariant_inner : Sequence[int], optional
             Widths of the inner MLP layers within the invariant module. Default is (64, 64).
         mlp_widths_invariant_outer : Sequence[int], optional
-            Widths of the outer MLP layers within the invariant module. Default is (64, 64).
+            Widths of the outer MLP layers within the invariant module. Default is (64, 4).
         mlp_widths_invariant_last : Sequence[int], optional
             Widths of the MLP layers in the final invariant transformation. Default is (64, 64).
         activation : str, optional

901d42be21d246979a37429913837412dd74fce7
Svenja Jedhoff <159010106+sjedhoff@users.noreply.github.com>
2025-10-31 13:19:49 +0100

    Adding z-score metric and fixing test_quantities problem in plot_quantity/pairs_quantity (#596)
    * Add posterior z score
    
    * Fixing test_quantities problem in plot_quantity/pairs_quantity
    
    * Removing unused variable found by code style check

--- tests/test_diagnostics/test_diagnostics_metrics.py
+++ tests/test_diagnostics/test_diagnostics_metrics.py
@@ -66,6 +66,26 @@ def test_posterior_contraction(random_estimates, random_targets):
     assert out["values"].shape[0] == len(test_quantities) + num_variables(random_estimates)
 
 
+def test_posterior_z_score(random_estimates, random_targets):
+    # basic functionality: automatic variable names
+    out = bf.diagnostics.metrics.posterior_z_score(random_estimates, random_targets)
+    assert list(out.keys()) == ["values", "metric_name", "variable_names"]
+    assert out["values"].shape == (num_variables(random_estimates),)
+    assert out["metric_name"] == "Posterior z-score"
+    assert out["variable_names"] == ["beta_0", "beta_1", "sigma"]
+    # test without aggregation
+    out = bf.diagnostics.metrics.posterior_z_score(random_estimates, random_targets, aggregation=None)
+    assert out["values"].shape == (random_estimates["sigma"].shape[0], num_variables(random_estimates))
+
+    # test quantities
+    test_quantities = {
+        r"$\beta_1 + \beta_2$": lambda data: np.sum(data["beta"], axis=-1),
+        r"$\beta_1 \cdot \beta_2$": lambda data: np.prod(data["beta"], axis=-1),
+    }
+    out = bf.diagnostics.metrics.posterior_z_score(random_estimates, random_targets, test_quantities=test_quantities)
+    assert out["values"].shape[0] == len(test_quantities) + num_variables(random_estimates)
+
+
 def test_root_mean_squared_error(random_estimates, random_targets):
     # basic functionality: automatic variable names
     out = bf.diagnostics.metrics.root_mean_squared_error(random_estimates, random_targets)

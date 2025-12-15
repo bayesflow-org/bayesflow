a3263ad40b38bb3117c7f0b17cee2bfd2d36c4db
arrjon <jonas.arruda@uni-bonn.de>
2025-12-15 22:29:56 +0100

    improve warnings

--- bayesflow/utils/optimal_transport/log_sinkhorn.py
+++ bayesflow/utils/optimal_transport/log_sinkhorn.py
@@ -11,7 +11,6 @@ from .ot_utils import (
     search_for_conditional_weight,
     auto_regularization,
 )
-from .. import logging
 
 
 def log_sinkhorn(x1: Tensor, x2: Tensor, conditions: Tensor | None = None, seed: int = None, **kwargs) -> Tensor:
@@ -101,10 +100,6 @@ def log_sinkhorn_plan(
 
     if regularization == "auto":
         regularization = auto_regularization(cost)
-        logging.debug(
-            f"Using regularization {float(keras.ops.convert_to_numpy(regularization))} (auto-tuned)"
-            f" for log Sinkhorn-Knopp OT."
-        )
     elif regularization <= 0.0:
         raise ValueError(f"regularization must be positive, got {regularization}")
 
@@ -169,19 +164,6 @@ def log_sinkhorn_plan(
 
     steps = 0
     steps, log_plan = keras.ops.while_loop(cond, body, (steps, log_plan), maximum_iterations=max_steps)
-
-    steps_value = int(keras.ops.convert_to_numpy(steps))
-    has_nans = bool(keras.ops.convert_to_numpy(contains_nans(log_plan)))
-    converged = bool(keras.ops.convert_to_numpy(is_converged(log_plan)))
-
-    if has_nans:
-        logging.warning(f"Log-Sinkhorn-Knopp produced NaNs after {steps_value} steps.")
-    elif converged:
-        logging.debug(f"Log-Sinkhorn-Knopp converged after {steps_value} steps.")
-    elif max_steps is not None and steps_value >= max_steps:
-        logging.warning(f"Log-Sinkhorn-Knopp did not converge after {max_steps} steps. ")
-    elif not converged:
-        logging.warning("Log-Sinkhorn-Knopp did not converge.")
 
     if partial:
         return log_plan[:-1, :-1]

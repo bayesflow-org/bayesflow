75f00a9ade2381bed79da361139297a78aadb4f9
Danie Habermann <133031176+daniel-habermann@users.noreply.github.com>
2025-12-31 05:28:13 +0100

    add unit tests for graphical_approximator.util.summary_input_shapes_by_network

--- tests/test_experimental/test_approximators/test_utils.py
+++ tests/test_experimental/test_approximators/test_utils.py
@@ -8,6 +8,7 @@ from bayesflow.experimental.graphical_approximator.utils import (
     inference_condition_shapes_by_network,
     inference_variable_shapes_by_network,
     data_condition_shapes_by_network,
+    summary_input_shapes_by_network,
 )
 import keras
 
@@ -151,7 +152,7 @@ def test_inference_condition_shapes_by_network_three_level(three_level_simulator
     #   'y': (2, N_classrooms, N_students, N_scores, 1)
     # }
     # summary output shapes:
-    # {0: (2, N_schools, N_students, 10), 1: (2, N_classrooms, 20), 2: (2, 30)}
+    # {0: (2, N_classrooms, N_students, 10), 1: (2, N_classrooms, 20), 2: (2, 30)}
     data = three_level_simulator.sample(2)
     data_shapes = three_level_approximator._data_shapes(data)
     expected_shapes = {
@@ -362,7 +363,7 @@ def test_data_condition_shapes_by_network_three_level(three_level_simulator, thr
     #   'y': (2, N_classrooms, N_students, N_scores, 1)
     # }
     # summary output shapes:
-    # {0: (2, N_schools, N_students, 10), 1: (2, N_classrooms, 20), 2: (2, 30)}
+    # {0: (2, N_classrooms, N_students, 10), 1: (2, N_classrooms, 20), 2: (2, 30)}
     data = three_level_simulator.sample(2)
     data_shapes = three_level_approximator._data_shapes(data)
     expected_shapes = {
@@ -413,3 +414,113 @@ def test_data_condition_shapes_by_network_crossed_design_irt(
     }
 
     assert data_condition_shapes_by_network(crossed_design_irt_approximator, data_shapes) == expected_shapes
+
+
+def test_summary_input_shapes_by_network_single_level(single_level_simulator, single_level_approximator):
+    # network composition:
+    # {0: ["prior"]}
+    # network conditions:
+    # {1: ["likelihood"]}
+    # data shapes:
+    # {
+    #   "beta": (2, 2),
+    #   "sigma": (2, 1),
+    #   "x": (2, N, 1),
+    #   "y": (2, N, 1),
+    # }
+    # summary output shapes:
+    # {0: (2, 10)}
+    data = single_level_simulator.sample(2)
+    data_shapes = single_level_approximator._data_shapes(data)
+    expected_shapes = {0: (2, data.meta["N"], 2)}
+
+    assert summary_input_shapes_by_network(single_level_approximator, data_shapes) == expected_shapes
+
+
+def test_summary_input_shapes_by_network_two_level(two_level_simulator, two_level_approximator):
+    # network composition:
+    # {0: ["hypers", "shared"], 1: ["locals"]}
+    # network conditions:
+    # {0: ["y"], 1: ["hypers", "shared", "y"]}
+    # data shapes:
+    # {
+    #   'hyper_mean': (2, 1),
+    #   'hyper_std': (2, 1),
+    #   'local_mean': (2, 6, 1),
+    #   'shared_std': (2, 1),
+    #   'y': (2, 6, 10, 1)
+    # }
+    # summary output shapes:
+    # {0: (2, 6, 10), 1: (2, 20)}
+    data_shapes = two_level_approximator._data_shapes(two_level_simulator.sample(2))
+    expected_shapes = {
+        0: (2, 6, 10, 1),
+        1: (2, 6, 10),
+    }
+
+    assert summary_input_shapes_by_network(two_level_approximator, data_shapes) == expected_shapes
+
+
+def test_summary_input_shapes_by_network_three_level(three_level_simulator, three_level_approximator):
+    # network composition:
+    # {0: ["schools, "shared"], 1: ["classrooms"], 2: ["students"]}
+    # network conditions:
+    # {0: ["scores"], 1: ["schools", "shared", "scores"], 2: ["schools", "classrooms", "shared", "scores"]}
+    # data shapes:
+    # {
+    #   'school_mu': (2, 1),
+    #   'school_sigma': (2, 1),
+    #   'classroom_mu': (2, N_classrooms, 1),
+    #   'classroom_sigma': (2, N_classrooms, 1),
+    #   'shared_sigma': (2, 1),
+    #   'student_mu': (2, N_classrooms, N_students, 1),
+    #   'student_sigma': (2, N_classrooms, N_students, 1),
+    #   'y': (2, N_classrooms, N_students, N_scores, 1)
+    # }
+    # summary output shapes:
+    # {0: (2, N_classrooms, N_students, 10), 1: (2, N_classrooms, 20), 2: (2, 30)}
+    data = three_level_simulator.sample(2)
+    data_shapes = three_level_approximator._data_shapes(data)
+    expected_shapes = {
+        0: (2, data.meta["N_classrooms"], data.meta["N_students"], data.meta["N_scores"], 1),
+        1: (2, data.meta["N_classrooms"], data.meta["N_students"], 10),
+        2: (2, data.meta["N_classrooms"], 20),
+    }
+
+    assert summary_input_shapes_by_network(three_level_approximator, data_shapes) == expected_shapes
+
+
+def test_summary_input_shapes_by_network_crossed_design_irt(
+    crossed_design_irt_simulator, crossed_design_irt_approximator
+):
+    # network composition:
+    # {0: ["schools"], 1: ["questions"], 2: ["students"]}
+    # network conditions:
+    # {0: ["observations"], 1: ["schools", "observations"], 2: ["schools", "questions", "observations"]}
+    # data shapes:
+    # {
+    #   'mu_question_mean': (2, 1),
+    #   'sigma_question_mean': (2, 1),
+    #   'mu_question_std': (2, 1),
+    #   'sigma_question_std': (2, 1),
+    #   'question_mean': (2, num_questions, 1),
+    #   'question_std': (2, num_questions, 1),
+    #   'question_difficulty': (2, num_questions, 1),
+    #   'student_ability': (2, num_students, 1),
+    #   'obs': (2, num_questions, num_students, 1)
+    # }
+    # summary output shapes:
+    # {0: (2, num_students, 10), 1: (2, 20)}
+    data = crossed_design_irt_simulator.sample(2)
+    data_shapes = crossed_design_irt_approximator._data_shapes(data)
+    expected_shapes = {
+        0: (
+            2,
+            data.meta["num_students"],
+            data.meta["num_questions"],
+            1,
+        ),  # num_questions and num_students swapped because questions are not amortizable
+        1: (2, data.meta["num_students"], 10),
+    }
+
+    assert summary_input_shapes_by_network(crossed_design_irt_approximator, data_shapes) == expected_shapes

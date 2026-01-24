1a3219f7e481c78ce60411be77beaba0d36fa58b
Danie Habermann <133031176+daniel-habermann@users.noreply.github.com>
2026-01-24 23:32:35 +0100

    wip GraphicalDataset

--- bayesflow/experimental/graphical_approximator/dataset.py
+++ bayesflow/experimental/graphical_approximator/dataset.py
@@ -1,13 +1,12 @@
 from collections.abc import Callable, Mapping, Sequence
-
+from bayesflow.utils import logging
 import keras
 import numpy as np
 
 from bayesflow.adapters import Adapter
-from bayesflow.simulators.simulator import Simulator
 
 from ..graphical_simulator import GraphicalSimulator
-from .utils import inference_variables_by_network, inference_conditions_by_network
+from .utils import inference_variables_by_network, inference_conditions_by_network, summary_inputs_by_network
 
 
 class GraphicalDataset(keras.utils.PyDataset):
@@ -17,11 +16,13 @@ class GraphicalDataset(keras.utils.PyDataset):
 
     def __init__(
         self,
-        simulator: GraphicalSimulator,
+        dataset,
+        simulator: GraphicalSimulator | None,
         approximator,
         batch_size: int,
         num_batches: int,
         adapter: Adapter | None,
+        num_samples=None,
         *,
         augmentations: Callable | Mapping[str, Callable] | Sequence[Callable] | None = None,
         **kwargs,
@@ -55,12 +56,20 @@ class GraphicalDataset(keras.utils.PyDataset):
         """
         super().__init__(**kwargs)
 
+        self.dataset = dataset
         self.simulator = simulator
         self.approximator = approximator
         self.batch_size = batch_size
         self._num_batches = num_batches
         self.adapter = adapter
         self.augmentations = augmentations or []
+        self.num_samples = None
+
+        if dataset and not self.num_samples:
+            self.num_samples = self._get_num_samples_from_data(dataset)
+            logging.debug(f"Automatically determined {self.num_samples} samples in data.")
+        elif not dataset:
+            self.num_samples = 10
 
     def __getitem__(self, index) -> dict[str, np.ndarray]:
         """
@@ -76,7 +85,18 @@ class GraphicalDataset(keras.utils.PyDataset):
         dict of str to np.ndarray
             A batch of simulated (and optionally augmented/adapted) data.
         """
-        batch = self.simulator.sample(self.batch_size)
+        if self.simulator:
+            batch = self.simulator.sample(self.batch_size)
+
+        if self.dataset:
+            num_samples = self._get_num_samples_from_data(self.dataset)
+            index = slice(index * self.batch_size, (index + 1) * self.batch_size)
+            index = np.arange(num_samples, dtype="int64")[index]
+
+            batch = {
+                key: np.take(value, index, axis=0) if isinstance(value, np.ndarray) else value
+                for key, value in self.dataset.items()
+            }
 
         if self.augmentations is None:
             pass
@@ -94,14 +114,24 @@ class GraphicalDataset(keras.utils.PyDataset):
         if self.adapter is not None:
             batch = self.adapter(batch)
 
-        print(inference_variables_by_network(self.approximator, batch))
         output = {
-            "__inference_variables": inference_variables_by_network(self.approximator, batch),
-            "__inference_conditions": inference_conditions_by_network(self.approximator, batch),
+            "summary_inputs": summary_inputs_by_network(self.approximator, dict(batch)),
+            "inference_variables": inference_variables_by_network(self.approximator, dict(batch)),
+            "inference_conditions": inference_conditions_by_network(self.approximator, dict(batch)),
         }
 
-        return output | batch
+        return output
 
     @property
-    def num_batches(self) -> int:
-        return self._num_batches
+    def num_batches(self) -> int | None:
+        return int(np.ceil(self.num_samples / self.batch_size))
+
+    @staticmethod
+    def _get_num_samples_from_data(data: Mapping) -> int:
+        for key, value in data.items():
+            if hasattr(value, "shape"):
+                ndim = len(value.shape)
+                if ndim > 1:
+                    return value.shape[0]
+
+        raise ValueError("Could not determine number of samples from data. Please pass it manually.")

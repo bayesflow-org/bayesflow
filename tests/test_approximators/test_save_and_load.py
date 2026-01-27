649e5afc3be2b4e0c65e6ebcb7c2dd317fa46ff6
han-ol <g@hans.olischlaeger.com>
2026-01-27 18:48:03 +0100

    use build_from_data and fix dataset in save&load test

--- tests/test_approximators/test_save_and_load.py
+++ tests/test_approximators/test_save_and_load.py
@@ -2,10 +2,14 @@ import keras
 from tests.utils import assert_models_equal
 
 
-def test_save_and_load(tmp_path, approximator, train_dataset, validation_dataset):
+def test_save_and_load(tmp_path, approximator, train_dataset):
+    from bayesflow import EnsembleApproximator, EnsembleDataset
+
+    if isinstance(approximator, EnsembleApproximator):
+        train_dataset = EnsembleDataset(train_dataset, num_ensemble=len(approximator.approximators))
+
     # to save, the model must be built
-    data_shapes = keras.tree.map_structure(keras.ops.shape, train_dataset[0])
-    approximator.build(data_shapes)
+    approximator.build_from_data(train_dataset[0])
     approximator.compute_metrics(**train_dataset[0])
 
     keras.saving.save_model(approximator, tmp_path / "model.keras")

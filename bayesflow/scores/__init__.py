dfb73e2eea78e89a8f3d88dc0634ad639d5880ef
han-ol <g@hans.olischlaeger.com>
2026-02-06 10:19:50 +0100

    Module docstring [no ci]

--- bayesflow/scores/__init__.py
+++ bayesflow/scores/__init__.py
@@ -1,4 +1,20 @@
-"""Scoring rules for point estimation."""
+r"""
+A collection of scoring rules for Bayes risk minimization with
+:py:class:`~bayesflow.networks.ScoringRuleInferenceNetwork`.
+
+Examples
+--------
+>>> # A network to estimate both point estimates and parameters of a multivariate normal distribution.
+>>> inference_network = bf.networks.ScoringRuleInferenceNetwork(
+        scores=dict(
+            mean=bf.scores.MeanScore(),
+            quantiles=bf.scores.QuantileScore(),
+            mvn=bf.scores.MultivariateNormalScore(),
+        )
+    )
+
+Inherit from :py:class:`ScoringRule` to build your own custom scoring rule.
+"""
 
 from .scoring_rule import ScoringRule
 from .parametric_distribution_score import ParametricDistributionScore

a3cb7c73ab7bd2a28f9acce73bde5e302fcac3f5
han-ol <g@hans.olischlaeger.com>
2026-02-17 10:46:30 +0100

    EnsembleApproximator.estimate is implemented now

--- tests/test_approximators/test_estimate.py
+++ tests/test_approximators/test_estimate.py
@@ -1,11 +1,8 @@
-import pytest
 import keras
 from tests.utils import check_combination_simulator_adapter
 
 
 def test_approximator_estimate(approximator, simulator, batch_size, adapter):
-    from bayesflow import EnsembleApproximator
-
     check_combination_simulator_adapter(simulator, adapter)
 
     num_batches = 4
@@ -16,12 +13,7 @@ def test_approximator_estimate(approximator, simulator, batch_size, adapter):
     batch_shapes = keras.tree.map_structure(keras.ops.shape, batch)
     approximator.build(batch_shapes)
 
-    # Check if approximator is an instance of EnsembleApproximator
-    if isinstance(approximator, EnsembleApproximator):
-        with pytest.raises(NotImplementedError):
-            approximator.estimate(data)
-    else:
-        estimates = approximator.estimate(data)
+    estimates = approximator.estimate(data)
 
-        assert isinstance(estimates, dict)
-        print(keras.tree.map_structure(keras.ops.shape, estimates))
+    assert isinstance(estimates, dict)
+    print(keras.tree.map_structure(keras.ops.shape, estimates))

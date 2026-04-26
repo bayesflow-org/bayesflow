c544e4766018e6e2dc992a9fb161fe57cda032e9
stefanradev93 <stefan.radev93@gmail.com>
2026-04-26 17:47:47 -0400

    Purge Residual

--- bayesflow/networks/helpers/__init__.py
+++ bayesflow/networks/helpers/__init__.py
@@ -1,6 +1,5 @@
 from .blocks import DenseBlock, ConditionalDenseBlock
 from .embeddings import FourierEmbedding, Time2Vec, RecurrentEmbedding, FiLM
-from .residual import Residual
 from .sequential import Sequential
 from .standardization import Standardization
 from .norms import SimpleNorm

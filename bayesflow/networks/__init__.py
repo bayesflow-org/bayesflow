bbffd7536fb5042214fe04b677f267116105cb49
Bhanu Prasanna <bhanu.prasanna2001@gmail.com>
2026-05-26 04:34:48 +0200

    latent inference network + rem latent diffusion

--- bayesflow/networks/__init__.py
+++ bayesflow/networks/__init__.py
@@ -21,7 +21,6 @@ from .inference import CouplingFlow
 from .inference import DiffusionModel
 from .inference import FlowMatching
 from .inference import LatentInferenceNetwork, Encoder, Decoder
-from .inference import LatentDiffusionModel
 from .inference import PointNetwork, ScoringRuleNetwork
 
 # Summary networks
--- bayesflow/networks/inference/__init__.py
+++ bayesflow/networks/inference/__init__.py
@@ -8,7 +8,6 @@ from .coupling import CouplingFlow
 from .diffusion import DiffusionModel
 from .flow_matching import FlowMatching
 from .latent import LatentInferenceNetwork, Encoder, Decoder
-from .latent_diffusion import LatentDiffusionModel
 from .scoring import ScoringRuleNetwork, PointNetwork
 from .scoring import (
     ScoringRule,

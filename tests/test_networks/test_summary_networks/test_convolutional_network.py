52081a8af6debcc270c57aee6ba443aa6cd65eb6
Aayush Mishra <aayush0006@gmail.com>
2026-05-28 11:31:28 +0200

    test for ConvNet kwargs forwarding

--- tests/test_networks/test_summary_networks/test_convolutional_network.py
+++ tests/test_networks/test_summary_networks/test_convolutional_network.py
@@ -133,3 +133,13 @@ def test_dropout():
     net = _make(dropout=0.1)
     y = net(_input(), training=False)
     assert keras.ops.shape(y) == (BATCH, SUMMARY_DIM)
+
+
+def test_convolutional_network_forwards_kwargs():
+    """
+    See: https://github.com/bayesflow-org/bayesflow/issues/699
+    """
+    from bayesflow.distributions import DiagonalNormal
+
+    net = ConvolutionalNetwork(base_distribution="normal")
+    assert isinstance(net.base_distribution, DiagonalNormal)

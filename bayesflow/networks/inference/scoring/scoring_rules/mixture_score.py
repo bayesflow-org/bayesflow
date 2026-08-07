df0047b403e2e4bbf47def129bc9d6d79e23179b
Hans Olischläger <106988117+han-ol@users.noreply.github.com>
2026-08-07 10:16:54 +0200

    skip needs to be tuple
    Multiple heads might need to be skipped in general.

--- bayesflow/networks/inference/scoring/scoring_rules/mixture_score.py
+++ bayesflow/networks/inference/scoring/scoring_rules/mixture_score.py
@@ -55,7 +55,7 @@ class MixtureScore(ParametricDistributionScore):
         )
     """
 
-    SKIP_INVERSE_ADAPTER = "mixture_logits"
+SKIP_INVERSE_ADAPTER = ("mixture_logits",)
 
     def __init__(
         self,

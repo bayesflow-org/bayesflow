27fa773c7da197f1d2e0dca1f7b785963f486a86
stefanradev93 <stefan.radev93@gmail.com>
2026-08-25 04:02:29 -0400

    Drop exposure of c2st helpers

--- bayesflow/diagnostics/metrics/__init__.py
+++ bayesflow/diagnostics/metrics/__init__.py
@@ -6,12 +6,7 @@ from .calibration_error import calibration_error
 from .posterior_contraction import posterior_contraction
 from .root_mean_squared_error import root_mean_squared_error
 from .expected_calibration_error import expected_calibration_error
-from .classifier_two_sample_test import (
-    area_under_the_curve,
-    classifier_two_sample_test,
-    conformal_test,
-    regression_statistic,
-)
+from .classifier_two_sample_test import classifier_two_sample_test
 from .model_misspecification import bootstrap_comparison, summary_space_comparison
 from .calibration_log_gamma import calibration_log_gamma, gamma_null_distribution, gamma_discrepancy
 from .posterior_z_score import posterior_z_score
